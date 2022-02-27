da16c4841fcb1524c5cdf2bf0243a119ac362999
Ayshanmaredia <58998373+Ayshanmaredia@users.noreply.github.com>
2022-02-27 16:49:20 -0500

    Deploy app

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.use(express.json());
 //Routes//
 //register and login routes
 
+app.use(express.static("public"));
+
 app.use("/auth", require("./routes/auth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
@@ -20,6 +22,6 @@ app.use("/invite", require("./routes/invite"));
 
 app.use("/member", require("./routes/member"));
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running");
 });
